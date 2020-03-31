e34cc9eba264887928d3aa19dadc428400c8c247
KarimLaachIAE <62889351+KarimLaachIAE@users.noreply.github.com>
2020-03-31 12:58:04 +0200

    v2.1_modificationTestJUnit

--- src/TestForm.java
+++ src/TestForm.java
@@ -14,23 +14,36 @@ import TypeEquipement.PaireSki;
 class TestForm {
 	
 	@Test
-	void equipementSeCreerBien() {
+	void chaussureSeCreerBien() {
 		Entreprise ent = new Entreprise();
 		
 		ArrayList<Chaussure> c1 = new ArrayList<Chaussure>();
 		ArrayList<Chaussure> c2 = new ArrayList<Chaussure>();
 		ArrayList<Chaussure> c3 = new ArrayList<Chaussure>();
 		
+		ent.creationCollectionChaussure(ent, c1, c2, c3);
+		
+		int i = (3*21);
+		int totalchaussure = c1.size() + c2.size() + c3.size();
+		assertEquals(totalchaussure, i);
+	}
+	
+	@Test
+	void skiSeCreerBien() {
+		Entreprise ent = new Entreprise();
+		
 		ArrayList<PaireSki> ps1 = new ArrayList<PaireSki>();
 		ArrayList<PaireSki> ps2 = new ArrayList<PaireSki>();
 		ArrayList<PaireSki> ps3 = new ArrayList<PaireSki>();
 		
-		ent.creationCollectionChaussure(ent, c1, c2, c3);
 		ent.creationCollectionSki(ent, ps1, ps2, ps3);
 		
-		int i = (3*21) + (3*101);
-		assertEquals(ent.afficherInfosEquipement().size(), i);
+		int i = (3*101);
+		int totalski = ps1.size() + ps2.size() + ps3.size();
+		assertEquals(totalski, i);
 	}
+	
+
 	
 
 }
